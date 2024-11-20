075b5546d802ec4677ee7ed18a63a2a25fc38bf6
Juan Galvez <juanjosegalvez2014@gmail.com>
2024-11-19 21:51:08 -0500

    V0.3 Added validation logic between 0-30 for contestans

--- GreenvilleRevenue/RevenueCalculator.cs
+++ GreenvilleRevenue/RevenueCalculator.cs
@@ -4,11 +4,34 @@ public class RevenueCalculator
 {
     public static void Calculate()
     {
-         Console.Write("Enter last year's number of contestants: ");
-        int lastYearContestants = int.Parse(Console.ReadLine());
+        int lastYearContestants = 0;
+        int currentYearContestants = 0;
 
-        Console.Write("Enter this year's number of contestants: ");
-        int currentYearContestants = int.Parse(Console.ReadLine());
+        while(true){
+
+            Console.Write("Enter last year's number of contestants: ");
+            lastYearContestants = int.Parse(Console.ReadLine());
+            if (lastYearContestants >= 0 && lastYearContestants <= 30){
+                break;
+            }
+            else{
+                Console.WriteLine("Please enter a number between 0 and 30");
+            }
+
+            
+        }
+
+        while(true){
+            Console.Write("Enter this year's number of contestants: ");
+            currentYearContestants = int.Parse(Console.ReadLine());
+            if(currentYearContestants >= 0 && currentYearContestants <= 30){
+                break;
+            }
+            else{
+                Console.WriteLine("Please enter a number between 0 and 30");
+            }
+        }
+
 
         // Calculation for the renenue
         int ticketCost = 25;
@@ -19,12 +42,12 @@ public class RevenueCalculator
         Console.WriteLine("Revenue expected this year is {0}", currentYearRevenue);
        
         // Comparison for contestant
-       if (currentYearContestants > 2 * lastYearContestants)
+       if (currentYearContestants == 2 * lastYearContestants)
         {
             Console.WriteLine("The competition is more than twice as big this year!");
         }
         else if (currentYearContestants > lastYearContestants)
-        {
+        { 
             Console.WriteLine("The competition is bigger than ever!");
         }
         else
